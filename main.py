565611c6050a608bf22f342831431147c2ce8e60
adenilsonrreis <reisadenilson300@gmail.com>
2026-05-29 19:49:58 -0300

    fix: corrige estilo flake8

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ def saudacao(nome: str) -> str:
         raise TypeError("Nome deve ser uma string")
     return f"Olá, {nome}!"
 
+
 def calcular_media(notas: list) -> float:
     if not notas:
         return 0
     return sum(notas) / len(notas)
 
+
 if __name__ == "__main__":
     print(saudacao("Aluno"))
     print(calcular_media([10, 8, 6]))
+    
