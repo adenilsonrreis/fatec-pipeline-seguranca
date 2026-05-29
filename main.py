5d39ccce1e46fc509275155210bbb55a98b9af10
adenilsonrreis <reisadenilson300@gmail.com>
2026-05-29 20:06:32 -0300

    fix: corrige padrao flake8

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sqlite3
+
+
 def saudacao(nome: str) -> str:
     if not isinstance(nome, str):
         raise TypeError("Nome deve ser uma string")
@@ -11,14 +13,15 @@ def calcular_media(notas: list) -> float:
     return sum(notas) / len(notas)
 
 
-if __name__ == "__main__":
-    print(saudacao("Aluno"))
-    print(calcular_media([10, 8, 6]))
-
 def buscar_usuario_vulneravel(user_id):
-    conn = sqlite3.connect('banco.db')
+    conn = sqlite3.connect("banco.db")
     cursor = conn.cursor()
 
     cursor.execute(f"SELECT * FROM users WHERE id={user_id}")
 
     return cursor.fetchone()
+
+
+if __name__ == "__main__":
+    print(saudacao("Aluno"))
+    print(calcular_media([10, 8, 6]))
