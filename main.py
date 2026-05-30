e405e7b708ca68b59001ae528430d77798a20003
adenilsonrreis <reisadenilson300@gmail.com>
2026-05-29 23:45:06 -0300

    fix: adiciona quebra de linha no final do arquivo

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sqlite3
-
-
 def saudacao(nome: str) -> str:
-    if not isinstance(nome, str):
+    dif not isinstance(nome, str):
         raise TypeError("Nome deve ser uma string")
     return f"Olá, {nome}!"
 
@@ -11,15 +8,6 @@ def calcular_media(notas: list) -> float:
     if not notas:
         return 0
     return sum(notas) / len(notas)
-
-
-def buscar_usuario_vulneravel(user_id):
-    conn = sqlite3.connect("banco.db")
-    cursor = conn.cursor()
-
-    cursor.execute(f"SELECT * FROM users WHERE id={user_id}")
-
-    return cursor.fetchone()
 
 
 if __name__ == "__main__":
